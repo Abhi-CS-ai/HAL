eb666b384034f1502cf1513b03c08301db3db16f
Abhishek Gupta <abhishekg17052002@gmail.com>
2024-11-20 17:54:48 +0100

    Update gpio-atmega328p.rs

--- gpio-atmega328p.rs
+++ gpio-atmega328p.rs
@@ -1,47 +1,41 @@
-#![no_std]
-#![no_main]
-
 use core::ptr;
+use crate::gpio::{GPIO, PinMode};
 
 const DDRB: *mut u8 = 0x24 as *mut u8;
 const PORTB: *mut u8 = 0x25 as *mut u8;
-const PINB: *mut u8 = 0x23 as *mut u8;
-/* [CORRECTION GPIO] 
-    Any operations here will be limited to the I/O registers of port B
-(Don't hesitate to remove this comment)*/
+const PINB: *const u8 = 0x23 as *const u8;
 
-#[derive(Clone, Copy)]
-pub enum PinMode {
-    Input,
-    Output,
-}
+pub struct Atmega328pGPIO;
 
-#[derive(Clone, Copy)]
-pub enum PinState {
-    High,
-    Low,
-}
+impl GPIO for Atmega328pGPIO {
+    fn set_pin_mode(pin: u8, mode: PinMode) {
+        unsafe {
+            match mode {
+                PinMode::Output => {
+                    ptr::write_volatile(DDRB, ptr::read_volatile(DDRB) | (1 << pin));
+                }
+                PinMode::Input => {
+                    ptr::write_volatile(DDRB, ptr::read_volatile(DDRB) & !(1 << pin));
+                }
+                PinMode::InputPullUp => {
+                    ptr::write_volatile(DDRB, ptr::read_volatile(DDRB) & !(1 << pin));
+                    ptr::write_volatile(PORTB, ptr::read_volatile(PORTB) | (1 << pin));
+                }
+            }
+        }
+    }
 
-fn modify_reg(reg: *mut u8, pin: u8, set: bool) {
-    let value = unsafe { ptr::read_volatile(reg) };
-    unsafe { ptr::write_volatile(reg, if set { value | (1 << pin) } else { value & !(1 << pin) }) };
-}
+    fn write_pin(pin: u8, value: bool) {
+        unsafe {
+            if value {
+                ptr::write_volatile(PORTB, ptr::read_volatile(PORTB) | (1 << pin));
+            } else {
+                ptr::write_volatile(PORTB, ptr::read_volatile(PORTB) & !(1 << pin));
+            }
+        }
+    }
 
-pub fn set_pin_mode(pin: u8, mode: PinMode) {
-    modify_reg(DDRB, pin, matches!(mode, PinMode::Output));
-}
-
-pub fn write_pin(pin: u8, state: PinState) {
-    modify_reg(PORTB, pin, matches!(state, PinState::High));
-}
-
-pub fn read_pin(pin: u8) -> PinState {
-    if unsafe { ptr::read_volatile(PINB) } & (1 << pin) != 0 {
-        PinState::High
-    } else {
-        PinState::Low
+    fn read_pin(pin: u8) -> bool {
+        unsafe { ptr::read_volatile(PINB) & (1 << pin) != 0 }
     }
 }
-
-#[no_mangle]
-pub extern "C" fn main() {}
